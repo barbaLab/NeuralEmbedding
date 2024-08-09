1c651e63bb60b3ca5034e728e3d58c3363e32cc6
Federico Barban <fedbarban@gmail.com>
2024-08-09 18:44:30 +0200

    Added UMAP files
    added umap files, updated install, updated gitignore

--- install.m
+++ install.m
@@ -1,5 +1,22 @@
-msetCCAgit = "https://github.com/lcparra/mcca.git";
 rootFolder = pwd;
-cd(fullfile(rootFolder,"+embedding/","+CCA/"));
-gitclone(msetCCAgit);
-movefile("mcca","+mcca");
+CCAfolder   = fullfile(rootFolder,"+embedding/","+CCA/+mcca");
+UMAPfolder  = fullfile(rootFolder,"+embedding/","+UMAP/umapAndEpp");
+tSNEfolder  = fullfile(rootFolder,"+embedding/","+tSNE/");
+
+if ~existsnotempty(CCAfolder)
+    msetCCAgit = "https://github.com/lcparra/mcca.git";
+    gitclone(msetCCAgit,CCAfolder);
+end
+
+if ~existsnotempty(UMAPfolder)
+    UMAProot = fullfile(rootFolder,"+embedding/","+UMAP");
+    zipfile = UMAPfolder+".zip";
+    unzip(zipfile,UMAPfolder);
+    addpath(genpath(UMAPfolder))
+end
+
+
+function value = existsnotempty(path)
+   value = logical(exist(path,"dir"));
+   value = value && (numel(dir(path)) > 2);
+end
