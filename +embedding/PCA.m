9c7baf90361a05bdb5f9c12193be900744905cc7
Federico Barban <fedbarban@gmail.com>
2024-08-31 15:41:27 +0200

    Added support for multiple Area masks in PCA

--- +embedding/PCA.m
+++ +embedding/PCA.m
@@ -1,9 +1,14 @@
 function [E,ProjMatrix,VarExplained] = PCA(D,pars)
+[nT,nA] = size(D);
+E = cell(nT,nA);
+ProjMatrix = cell(1,nA);
+VarExplained = cell(1,nA);
 
-D_ = repmat(struct(),numel(D),1);
-for id=1:numel(D)
-   D_(id).data = full(D{id});
+for am = 1:size(D,2)
+    D_ = repmat(struct(),size(D,1),1);
+    for id=1:size(D,1)
+        D_(id).data = full(D{id,am});
+    end
+    [E(:,am),ProjMatrix(:,am),VarExplained(:,am)] = embedding.PCA.reduce(D_,pars.numPC);
 end
-
-[E,ProjMatrix,VarExplained] = embedding.PCA.reduce(D_,pars.numPC);
 end
