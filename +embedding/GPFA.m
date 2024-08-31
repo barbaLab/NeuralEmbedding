e5f3807fa770e4f7ed2046c8772099ea2755a5d5
Federico Barban <fedbarban@gmail.com>
2024-08-31 15:41:06 +0200

    Changed how testSequence is dealt with in GPFA. Now it's easier to run the method without tests

--- +embedding/GPFA.m
+++ +embedding/GPFA.m
@@ -1,20 +1,25 @@
 function [E,ProjMatrix,VarExplained]= GPFA(D,pars)
 
 seqTest = pars.seqTest;
-if isempty(seqTest)
-    % if test sequence is not defined, randomly extract 5% of all trials to
-    % be test trials
-    nTrial = length(D);
-    seqTest = false(1,nTrial);
-    seqTest(randperm(nTrial,floor(nTrial * .05))) = true;
+nTrial = length(D);
+if pars.doTest
+    if isempty(seqTest)
+        % if test sequence is not defined, randomly extract 5% of all trials to
+        % be test trials
+        seqTest = false(nTrial,1);
+        seqTest(randperm(nTrial,floor(nTrial * .05))) = true;
+    end
+else
+    seqTest = false(nTrial,1);
 end
+pars = rmfield(pars,"seqTest");
 
-D_ = repmat(struct(),numel(D));
+D_ = repmat(struct(),numel(D),1);
 for id=1:numel(D)
     D_(id).y = full(D{id});
     D_(id).trialId = id;
 end
-[D_.T] = deal(pars.TrialL);
+[D_.T] = deal(pars.TrialL{:});
 otherArgs = [fieldnames(pars) struct2cell(pars)]';
 [E,ProjMatrix,VarExplained] = embedding.GPFA.reduce(D_(~seqTest),D_(seqTest),...
     'xDim',pars.numPC,'verbose',false,'binWidth',pars.subsampling,...
